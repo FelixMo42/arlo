cd1010948114630669be270d680cbd2e81bf385d
Felix Moses <felix@tacocat.com>
2025-05-09 23:35:03 +0200

    use .json for cache files

--- src/cache.ts
+++ src/cache.ts
@@ -28,5 +28,5 @@ async function getCachePath(request: any): Promise<string> {
     const hashBuffer = await crypto.subtle.digest('SHA-256', data)
     const hashArray = Array.from(new Uint8Array(hashBuffer))
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('')
-    return `./out/.cache/${hashHex}.cache`
+    return `./out/.cache/${hashHex}.json`
 }
